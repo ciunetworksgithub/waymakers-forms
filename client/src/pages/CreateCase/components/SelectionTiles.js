fcfb3cec67a338750d7284df33945b79c2756fd6
Jason Gordon <jason.m.gordon@gmail.com>
2024-01-14 23:51:26 -0800

    feat: move tile definitions into config

--- client/src/pages/CreateCase/components/SelectionTiles.js
+++ client/src/pages/CreateCase/components/SelectionTiles.js
@@ -3,39 +3,9 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-import './SelectionTiles.css';
+import tileDefs from '../config/tile-definitions.json';
 
-const tiles = [
-  [
-    {
-      account: 'Bullfrogs',
-      description: 'I need my network fixed',
-      subject: 'Networking',
-    },
-    {
-      account: 'Bullfrogs with computers',
-      description: 'I need my computer fixed',
-      subject: 'Desktop Issue',
-    },
-    {
-      account: 'Bullfrogs with cellies',
-      description: 'I need my cell phone fixed',
-      subject: 'Cell Phone Issue',
-    },
-  ],
-  [
-    {
-      account: 'Bullfrogs with friends',
-      description: 'I need my social life fixed',
-      subject: 'Personnel',
-    },
-    {
-      account: 'Friendless Bullfrogs',
-      description: 'I need some friends',
-      subject: 'Other',
-    },
-  ],
-];
+import './SelectionTiles.css';
 
 const Tile = ({ onClick, children }) => {
   return (
@@ -47,16 +17,14 @@ const Tile = ({ onClick, children }) => {
 
 export const SelectionTiles = ({ onComplete }) => {
   return (
-    <Container className="SelectionTiles h-100 d-inline-block">
-      {tiles.map((row, rowIndex) => (
-        <Row key={rowIndex} className="mb-3">
-          {row.map((data, colIndex) => (
-            <Col key={colIndex}>
-              <Tile onClick={() => onComplete(data)}>{data.subject}</Tile>
-            </Col>
-          ))}
-        </Row>
-      ))}
+    <Container className="SelectionTiles h-100">
+      <Row>
+        {tileDefs.map((tileDef, idx) => (
+          <Col key={idx}>
+            <Tile key={idx} onClick={() => onComplete(tileDef)}>{tileDef.subject}</Tile>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 };
