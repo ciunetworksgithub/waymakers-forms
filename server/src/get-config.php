4dff851db4a9dfd9c014879c21334ff3dae7f5fd
Jason Gordon <jason.m.gordon@gmail.com>
2024-03-16 11:57:36 -0700

    feat: merge HR tiles config and "regular" tiles config

--- server/src/get-config.php
+++ server/src/get-config.php
@@ -7,7 +7,9 @@ function read_config($name)
   if ($_GET['path'] === "/HR") {
     $filepath = $path . "/HR/" . $name . ".json";
     if (file_exists($filepath)) {
-      return file_get_contents($filepath);
+      $hr_config = json_decode(file_get_contents($filepath));
+      $config = json_decode(file_get_contents($path . "/" . $name . ".json"));
+      return json_encode(array_merge($hr_config, $config));
     }
   }
   return file_get_contents($path . "/" . $name . ".json");
