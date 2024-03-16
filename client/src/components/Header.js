fb94f1660bfcb1e4893ef96d019b3d8cac09fd3f
Jason Gordon <jason.m.gordon@gmail.com>
2024-03-16 12:22:34 -0700

    feat: add suffix to page header title

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,11 +2,13 @@ import Image from 'react-bootstrap/Image';
 
 import './Header.css';
 
+const getTitleSuffix = () => window.location.pathname.replace(/\//, '');
+
 export const Header = () => {
   return (
     <h1 className="Header mt-3 mb-3">
       <Image width="150" src="/img/logo.jpg" />
-      Support Center
+      Support Center {getTitleSuffix()}
     </h1>
   );
 };
